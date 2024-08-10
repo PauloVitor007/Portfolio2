5376d24c56d16645e2794d33da62b18a54b12470
PauloVitor007 <paulo.hermogens@academico.ifpb.edu.br>
2024-08-10 12:13:58 +0000

    teste

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/opaa.avif";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Eu sou formado em Informatica pela escola ECIT CUITÉ, possuo experiencias de trabalho na área de Telecomunição, pois atueie como estagiario na empresa "Agility Telecom" por volta de um ano.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Design",
+  "Programação basica em Java e Python",
+  "Conquistar um ",
+  "Possuo otima desenvoltura para realizar trabalhos em equipe",
+  "Ultilizo tanto interfaces mobile como desktop",
+  "Conheçimento basico em edição de video",
 ];
 
 /**
